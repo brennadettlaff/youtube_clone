a97172c3c61569b6e7822b29c7ef0856846fbe3c
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-09-23 15:50:36 -0600

    cleaned up code in comment and reply view files

--- backend/reply/views.py
+++ backend/reply/views.py
@@ -1,11 +1,7 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
-from comment.serializers import CommentSerializer
-from comment.models import Comment
 from .serializers import ReplySerializer
 from .models import Reply
 
