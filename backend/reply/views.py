10a7ba25fb45bb2fed0c86566e9336ddb312dc9e
Jalen Arrington <jmarrington178@yahoo.com>
2022-09-23 15:25:10 -0400

    Created urls.py in the reply app and added the reply_list view function to the reply app views file

--- backend/reply/views.py
+++ backend/reply/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from comment.serializers import CommentSerializer
+from comment.models import Comment
+from .serializers import ReplySerializer
+from .models import Reply
 
 # Create your views here.
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def reply_list(request):
+    if request.method == 'GET':
+        comments = Comment.objects.filter(user_id=request.comment.id)
+        serializer = ReplySerializer(comments, many=True)
+        return Response(serializer.data)
