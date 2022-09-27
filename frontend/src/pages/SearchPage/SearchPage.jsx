2ab5f9e77fb9ccc73454f20f7a69860a2b0cf009
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-09-27 13:49:23 -0600

    added functionality to display searched videos thumbnails

--- frontend/src/pages/SearchPage/SearchPage.jsx
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -4,8 +4,33 @@ import './SearchPage'
 
 
 const SearchPage = (props) => {
+
+  const [searchedVids, setSearchedVids] = useState([])
+  
+  let searchURL = "https://www.googleapis.com/youtube/v3/search?q=" + props.data + "&key=AIzaSyBQVFM2XjDbQQtS3T0MdPfoGmgcztrBD50&part=snippet"
+
+  useEffect(() => {
+    fetchSearchedVideos();
+  }, []);
+
+  const fetchSearchedVideos = async () => {
+    let response = await axios.get(searchURL)
+    debugger
+    console.log("Youtube search response: ",response.data)
+    setSearchedVids(response.data.items)
+  };
+
+
+
+
   return (
     <div>
+      {searchedVids.map((element) => {
+        return (
+          <img src = {element.snippet.thumbnails.default.url} />
+        )
+      })}
+{/*       
       {console.log(props.data)}
       <iframe
         id="ytplayer"
@@ -14,7 +39,7 @@ const SearchPage = (props) => {
         height="360"
         src={"https://www.youtube.com/embed/" + props.data + "?autoplay=1&origin=http://example.com"}
         frameborder="0"
-      ></iframe>
+      ></iframe> */}
     </div>
   );
 }
