2b2823536f9ac97f2f2d3f8fd67c7bd3b7d97c76
Jalen Arrington <jmarrington178@yahoo.com>
2022-09-28 15:50:09 -0400

    Restructured reply pages to mimic our comment pages

--- frontend/src/components/Reply/Reply.jsx
+++ frontend/src/components/Reply/Reply.jsx
@@ -2,34 +2,22 @@ import React, { useState, useEffect } from 'react';
 
 const Reply = (props) => {
     
-    // Use Effect and async function will need to be moved to app.js file
-    useEffect(() => {
-        getAllReplies();
-    }, [])
+    // // Use Effect and async function will need to be moved to app.js file
+    // useEffect(() => {
+    //     getAllReplies();
+    // }, [])
     
-    async function getAllReplies(){
-        let response = await axios.get('http://127.0.0.1:8000/api/view/<int:comment>/');
-        setReplies(response.data);
-    }
-    //
-
-    const [user, setUser] = useState('');
-    const [comment_id, setComment_id] = useState('');
-    const [text, setText] = useState('');
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        let newReply = {
-            user: user,
-            comment_id: comment_id,
-            text: text,
-        }
-        console.log(newReply)
-        props.addNewReplyProperty(newReply)
-    }
+    // async function getAllReplies(){
+    //     let response = await axios.get('http://127.0.0.1:8000/api/view/<int:comment>/');
+    //     setReplies(response.data);
+    // }
+    // //
 
     return ( 
-        console.log(newReply)
+        <div>
+            <h1>Replies</h1>
+            <ReplyForm />
+        </div>
      );
 }
  
--- frontend/src/components/ReplyForm/ReplyForm.jsx
+++ frontend/src/components/ReplyForm/ReplyForm.jsx
@@ -1,15 +1,35 @@
 import React, { useState } from 'react';
 import Reply from '../Reply/Reply';
+import Comment from '../Comment/Comment';
+import ReplyList from '../ReplyList/ReplyList';
 
 const ReplyForm = (props) => {
     
+    const [user, setUser] = useState('');
+    const [comment_id, setComment_id] = useState('');
+    const [text, setText] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newReply = {
+            user: user,
+            comment_id: comment_id,
+            text: text,
+        }
+        
+    }
+
     return ( 
         <form onSubmit={handleSubmit} className='form-grid'>
-            <label>User</label>
-            <input type='text' placeholder="User..." style={{margin: '5px'}} value={title} onChange={(event) => setUser(event.target.value)} />
-            <label>Text</label>
-            <input type='text' placeholder="Text..." style={{margin: '5px'}} value={album} onChange={(event) => setText(event.target.value)} />
-            <button type='submit' className='btn btn-secondary' style={{margin: '5px'}}>Add Song</button>
+            <div>
+                <label>User</label>
+                <input type='text' placeholder="User..." style={{margin: '5px'}} value={user} onChange={(event) => setUser(event.target.value)} />
+            </div>
+            <div>
+                <label>Reply</label>
+                <input type='text' placeholder="Reply..." style={{margin: '5px'}} value={reply} onChange={(event) => setReply(event.target.value)} />
+            </div>
+            <button type='submit' className='btn btn-secondary' style={{margin: '5px'}}>Add Comment</button>
         </form>
      );
 }
